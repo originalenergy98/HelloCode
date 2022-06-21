ed2575919963dd0ab950198516348ab077478bd6
Aleksandra <igoshina.sashenka98@gmail.com>
2022-06-22 02:14:11 +0700

    Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Маша") //== равен .ToLower перевод всех букв в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
